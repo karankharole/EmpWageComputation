381cda912c27c74476046d78973ca1eff8e404bd
karankharole <karankharole2000@gmail.com>
2022-10-28 21:07:50 +0530

    check if employee is part time or full time

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-        
+            // Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
 
-          
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            // Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
